617d052bc7026a2a07b1a8c344fb73b74a4fae81
nasim1717 <98159877+nasim1717@users.noreply.github.com>
2024-03-08 17:41:48 +0600

    favourite features implement

--- src/components/Pages/SingleBlog/Floating.jsx
+++ src/components/Pages/SingleBlog/Floating.jsx
@@ -1,6 +1,9 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import { actions } from "../../../actions";
 import CommentIcon from "../../../assets/icons/comment.svg";
+import HartFilledIcon from "../../../assets/icons/heart-filled.svg";
 import HeartIcon from "../../../assets/icons/heart.svg";
 import LikeFilledIcon from "../../../assets/icons/like-filled.svg";
 import LikeIcon from "../../../assets/icons/like.svg";
@@ -9,12 +12,37 @@ import { useAxios } from "../../../hooks/useAxios";
 import { useSingleBlog } from "../../../hooks/useSingleBlog";
 
 export default function Floating() {
+  const { blogId } = useParams();
   const { auth, setAuth } = useAuth();
   const { singleBlog, dispatch } = useSingleBlog();
   const { api } = useAxios();
+  const [isFavourite, setIsFavourite] = useState(false);
+
+  // get favourite api handle
+  useEffect(() => {
+    const fetchFavouriteBlogs = async () => {
+      try {
+        const response = await api.get(`/blogs/favourites`);
+        if (response.status === 200) {
+          const data = response?.data?.blogs;
+          const isFavouriteFind = data?.find((favourite) => favourite?.id === blogId);
+          if (isFavouriteFind) {
+            setIsFavourite(true);
+          } else {
+            setIsFavourite(false);
+          }
+        }
+      } catch (error) {
+        setAuth({});
+        localStorage.removeItem("auth");
+      }
+    };
+    fetchFavouriteBlogs();
+  }, []);
 
   const isLike = singleBlog?.likes?.find((like) => like?.id === auth?.user?.id);
 
+  // like features implement
   const handleLike = async (id) => {
     if (auth?.user) {
       try {
@@ -33,18 +61,23 @@ export default function Floating() {
     }
   };
 
-  const handleHart = () => {
+  // fevourite features implement
+  const handleHart = async (id) => {
     if (auth?.user) {
       try {
+        const response = await api.patch(`/blogs/${id}/favourite`);
+        if (response.status === 200) {
+          setIsFavourite(response?.data?.isFavourite);
+        }
       } catch (error) {
-        console.log(error);
+        localStorage.removeItem("auth");
+        setAuth({});
+        toast.warning("Login first then favourite", { autoClose: 3000 });
       }
     } else {
       toast.warning("Login first then favourite", { autoClose: 3000 });
     }
   };
-
-  console.log("islike-->", isLike);
 
   return (
     <div className="floating-action">
@@ -53,8 +86,8 @@ export default function Floating() {
           <img src={isLike ? LikeFilledIcon : LikeIcon} alt="like" />
           <span>{singleBlog?.likes?.length > 0 && singleBlog?.likes?.length}</span>
         </li>
-        <li onClick={handleHart}>
-          <img src={HeartIcon} alt="Favourite" />
+        <li onClick={() => handleHart(singleBlog?.id)}>
+          <img src={isFavourite ? HartFilledIcon : HeartIcon} alt="Favourite" />
         </li>
         <a href="#comments">
           <li>
